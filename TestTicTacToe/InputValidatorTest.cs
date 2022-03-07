9d8147233b3a7ddf6d3923dc32219772aff45701
sunnykath <suaysh.kathura@myob.com>
2022-03-08 11:34:12 +1300

    Updated the regex to be stricter

--- TestTicTacToe/InputValidatorTest.cs
+++ TestTicTacToe/InputValidatorTest.cs
@@ -7,11 +7,11 @@ namespace TestTicTacToe
     public class InputValidatorTest
     {
         [Theory]
-        [InlineData("3,3", "[1-3],[1-3]")]
-        [InlineData("1,1", "[1-3],[1-3]")]
-        [InlineData("2,2", "[1-3],[1-3]")]
-        [InlineData("1,3", "[1-3],[1-3]")]
-        public void Test_Should_Equal_True_If_Input_Is_Valid(string input, string regex)
+        [InlineData("3,3", "^[1-3],[1-3]$")]
+        [InlineData("1,1", "^[1-3],[1-3]$")]
+        [InlineData("2,2", "^[1-3],[1-3]$")]
+        [InlineData("1,3", "^[1-3],[1-3]$")]
+        public void Should_Equal_True_If_Input_Is_Valid(string input, string regex)
         {
             // Act
             var result = InputValidator.CheckInput(regex, input);
@@ -20,17 +20,16 @@ namespace TestTicTacToe
         }
 
         [Theory]
-        [InlineData("3,4", "[1-3],[1-3]")]
-        [InlineData("5,5", "[1-3],[1-3]")]
-        [InlineData("3,4", "[1-3] [1-3]")]
-        // [InlineData("", "^[1-3],[1-3]$")]
-        // [InlineData(",", "^[1-3],[1-3]$")]
-        [InlineData("1,a", "[1-3],[1-3]")]
-        [InlineData("sun", "[1-3],[1-3]")]
-        public void Test_Should_Equal_False_If_Input_Is_Invalid(string input, string regex)
+        [InlineData("3,4", "^[1-3],[1-3]$")]
+        [InlineData("5,5", "^[1-3],[1-3]$")]
+        [InlineData("", "^[1-3],[1-3]$")]
+        [InlineData(",", "^[1-3],[1-3]$")]
+        [InlineData("1,a", "^[1-3],[1-3]$")]
+        [InlineData("sun", "^[1-3],[1-3]$")]
+        public void Should_Equal_False_If_Input_Is_Invalid(string input, string regex)
         {
             // Act
-            var result = InputValidator.CheckInput(input, regex);
+            var result = InputValidator.CheckInput(regex, input);
             //Assert
             Assert.False(result);
         }
