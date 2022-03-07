9cac63a51d8e6d3af032ef7daf05c2ecd5b9623d
sunnykath <suaysh.kathura@myob.com>
2022-03-08 11:42:24 +1300

    Added test to check overriding of marker placements

--- TestTicTacToe/BoardTests.cs
+++ TestTicTacToe/BoardTests.cs
@@ -27,5 +27,31 @@ namespace TestTicTacToe
             Assert.True(success);
             Assert.Equal(expectedBoard, actualBoard);
         }
+        
+        
+        [Fact]
+        public void Should_Not_Place_Marker_In_The_Position_If_Cell_Not_Empty()
+        {
+            // Arrange 
+            var board = new Board();
+            var firstMove = new Move(1, 1, 1);
+            var secondMove = new Move(1, 1, 2);
+            
+            var expectedBoard = new int[3,3]
+            {
+                {1, 0, 0},
+                {0, 0, 0},
+                {0, 0, 0}
+            };
+
+            // Act
+            board.PlaceMarker(firstMove);    
+            var success = board.PlaceMarker(secondMove);    
+            var actualBoard = board.GetBoard();
+        
+            // Assert
+            Assert.True(success);
+            Assert.Equal(expectedBoard, actualBoard);
+        }
     }
 }
