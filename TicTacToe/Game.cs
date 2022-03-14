30c421af280420cdcc697fa9e10914af2496d367
sunnykath <suaysh.kathura@myob.com>
2022-03-14 13:36:13 +1300

    Imported Game Status checks

--- TicTacToe/Game.cs
+++ TicTacToe/Game.cs
@@ -3,6 +3,8 @@ namespace TicTacToe
     public class Game
     {
         private int _playerTurn;
+        private GameStatus _gameStatus;
+        private Board _board;
         
         public Game()
         {
@@ -44,6 +46,59 @@ namespace TicTacToe
         {
             
         }
+        
+        
+        public GameStatus CheckGameStatus(Board board)
+        {
+            _gameStatus = HasWon() ? GameStatus.Won : (HasDrawn() ? GameStatus.Draw : GameStatus.Playing);
+            
+            return _gameStatus;
+        }
+
+        private bool HasDrawn()
+        {
+            
+        }
+        
+        private bool HasWon()
+        {
+            var boardArray = _board.GetBoard();
+            return IsARowWin(boardArray) || IsAColumnWin(boardArray) || IsADiagonalWin(boardArray);
+        }
+        private static bool IsARowWin(int[,] boardArray)
+        {
+            for (var i = 0; i < boardArray.GetLength(0); i++)
+            {
+                if (CheckEquivalence(boardArray[i, 0], boardArray[i, 1], boardArray[i, 2]))
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+        private static bool IsAColumnWin(int[,] boardArray)
+        {
+            for (var i = 0; i < boardArray.GetLength(1); i++)
+            {
+                if (CheckEquivalence(boardArray[0,i], boardArray[1,i], boardArray[2,i]))
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+        private static bool IsADiagonalWin(int[,] boardArray)
+        {
+            return CheckEquivalence(boardArray[1, 1], boardArray[2, 2],boardArray[0, 0]) 
+                   || CheckEquivalence(boardArray[1, 1], boardArray[2, 0], boardArray[0, 2]);
+        }
+
+        private static bool CheckEquivalence(int a, int b, int c)
+        {
+            return (a == b) && (b == c);
+        }
+        
+        
         
     }
 }
