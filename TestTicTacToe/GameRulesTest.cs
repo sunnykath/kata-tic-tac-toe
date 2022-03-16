e27249e8621ac139c920fb82c468e8cad13e3393
sunnykath <suaysh.kathura@myob.com>
2022-03-17 11:25:18 +1300

    Added false tests for Diagonal wins

--- TestTicTacToe/GameRulesTest.cs
+++ TestTicTacToe/GameRulesTest.cs
@@ -43,6 +43,44 @@ namespace TestTicTacToe
             Assert.False(hasWonGame);
         }
 
+        [Fact]
+        public void Should_Return_False_When_3_In_A_Row_Diagonally_As_Forward_Slash_Of_Different_Players_Marks()
+        {
+            // Arrange 
+            var gameLosingBoard = new int[3,3]
+            {
+                {2, 0, 0},
+                {0, 1, 0},
+                {0, 0, 1}
+            };
+        
+        
+            // Act
+            var hasWonGame = GameRulesHandler.HasWon(gameLosingBoard);
+        
+            // Assert
+            Assert.False(hasWonGame);
+        }
+        
+        [Fact]
+        public void Should_Return_False_When_3_In_A_Row_Diagonally_As_Backward_Slash_Of_Different_Players_Marks()
+        {
+            // Arrange 
+            var gameLosingBoard = new int[3,3]
+            {
+                {0, 0, 1},
+                {0, 2, 0},
+                {1, 0, 0}
+            };
+        
+        
+            // Act
+            var hasWonGame = GameRulesHandler.HasWon(gameLosingBoard);
+        
+            // Assert
+            Assert.False(hasWonGame);
+        }
+        
         
         [Fact]
         public void Should_Return_Won_When_GameStatus_Is_Checked_If_3_In_A_Row_Horizontally()
