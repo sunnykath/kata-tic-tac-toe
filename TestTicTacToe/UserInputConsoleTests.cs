991bc6c82f166378ffa1e55557ca2a7712150ce5
sunnykath <suaysh.kathura@myob.com>
2022-03-17 11:28:12 +1300

    Implemented user input tests for valid and invalid inputs

---
+++ TestTicTacToe/UserInputConsoleTests.cs
@@ -0,0 +1,65 @@
+using System;
+using System.IO;
+using TicTacToe;
+using Xunit;
+
+namespace TestTicTacToe
+{
+    public class UserInputConsoleTests
+    {
+
+
+        [Fact]
+        public void Should_Ask_For_Input_Again_When_Invalid_Input_Is_Given_For_The_Players_Move()
+        {
+            
+            // Arrange
+            var stringWriter = new StringWriter();
+            Console.SetOut(stringWriter);
+
+            const string expectedStringContains = "Invalid Input, please try again: ";
+            
+            var stringReader = new StringReader("1 1\n1,1");
+            Console.SetIn(stringReader);
+            
+            var uiConsole = new UserInputConsole();
+            
+            // Act
+            uiConsole.GetPlayerMove(1);
+            var actualString = stringWriter.ToString();
+            
+            // Assert
+            Assert.True(actualString.Contains(expectedStringContains));
+            
+        }
+        
+        [Fact]
+        public void Should_Return_The_Move_When_Valid_Move_Input_Is_Given()
+        {
+            
+            // Arrange
+            var stringWriter = new StringWriter();
+            Console.SetOut(stringWriter);
+
+            var expectedStringContains = "Move accepted";
+            
+            var stringReader = new StringReader("1,2");
+            Console.SetIn(stringReader);
+
+            var expectedMove = new Move(1, 2);
+            
+            var uiConsole = new UserInputConsole();
+            
+            // Act
+            var actualMove = uiConsole.GetPlayerMove(1);
+            var actualString = stringWriter.ToString();
+            
+            // Assert
+            Assert.True(actualString.Contains(expectedStringContains));
+            Assert.Equal(expectedMove, actualMove);
+            
+        }
+        
+        
+    }
+}
