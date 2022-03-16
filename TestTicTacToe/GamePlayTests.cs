fae9315c7d6437477b8eb70c7db21196d6bae5bb
sunnykath <suaysh.kathura@myob.com>
2022-03-17 11:45:54 +1300

    Added test to check if player is swapped back to the initial player after 2 swaps

--- TestTicTacToe/GamePlayTests.cs
+++ TestTicTacToe/GamePlayTests.cs
@@ -6,9 +6,8 @@ namespace TestTicTacToe
     public class GamePlayTests
     {
 
-
         [Fact]
-        public void Should_Change_Player_When_SwapPlayer_IS_Called()
+        public void Should_Change_Player_When_SwapPlayer_Is_Called()
         {
             // Arrange 
             var game = new GameLoop();
@@ -22,7 +21,24 @@ namespace TestTicTacToe
             Assert.NotEqual(initialPlayer, swappedPlayer);
 
         }
-        
+
+        [Fact]
+        public void Should_Change_Player_To_The_Initial_Player_When_SwapPlayer_Is_Called_Twice()
+        {
+            // Arrange 
+            var game = new GameLoop();
+            
+            // Act
+            var initialPlayer = game.GetPlayer();
+            game.SwapPlayer();
+            game.SwapPlayer();
+            var swappedPlayer = game.GetPlayer();
+
+            // Assert 
+            Assert.Equal(initialPlayer, swappedPlayer);
+
+        }
+
         
         
         
