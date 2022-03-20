bae4048e2d1e445c3c0fe8ee5645a16ec9d15048
sunnykath <suaysh.kathura@myob.com>
2022-03-21 11:12:45 +1300

    Implemented the quit option for user input

--- TicTacToe/UserInput/UserInputConsole.cs
+++ TicTacToe/UserInput/UserInputConsole.cs
@@ -7,34 +7,42 @@ namespace TicTacToe
     {
         private readonly string _moveRegex = "^[1-3],[1-3]$";
         private Move moveInput;
+        private bool hasGivenUp;
 
         public void GetPlayerInput(int player)
         {
             Console.Write($"Player {player} enter a coord x,y to place your X or enter 'q' to give up: ");
 
             var input = Console.ReadLine();
-            // @TODO: Check for q
-            
-            var isValidInput = InputValidator.CheckInput(_moveRegex, input);
-            while (!isValidInput)
-            {
-                Console.Write("Invalid Input, please try again: ");
-                input = Console.ReadLine();
-                isValidInput = InputValidator.CheckInput(_moveRegex, input);
-            }
-            
-            Console.Write("Move accepted, ");
 
-            var inputStrings = input.Split(',');
-            var moveCoords = inputStrings.Select(int.Parse).ToList();
-            
-            moveInput = new Move(moveCoords[0], moveCoords[1]);
+            if (input == "q")
+            {
+                hasGivenUp = true;
+                Console.Write($"Player {player} has given up.");
+            }
+            else
+            {
+
+                var isValidInput = InputValidator.CheckInput(_moveRegex, input);
+                while (!isValidInput)
+                {
+                    Console.Write("Invalid Input, please try again: ");
+                    input = Console.ReadLine();
+                    isValidInput = InputValidator.CheckInput(_moveRegex, input);
+                }
+
+                Console.Write("Move accepted, ");
+
+                var inputStrings = input.Split(',');
+                var moveCoords = inputStrings.Select(int.Parse).ToList();
+
+                moveInput = new Move(moveCoords[0], moveCoords[1]);
+            }
         }
 
         public bool PlayerHasGivenUp()
         {
-
-            return true;
+            return hasGivenUp;
         }
         
         public Move GetPlayerMove()
