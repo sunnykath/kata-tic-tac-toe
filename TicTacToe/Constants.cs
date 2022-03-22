2f224436d036fc12fb6cf26f77427c0d952abae6
sunnykath <suaysh.kathura@myob.com>
2022-03-22 17:41:44 +1300

    Adding the index adjustment constant for translating the user input to the board indexes

--- TicTacToe/Constants.cs
+++ TicTacToe/Constants.cs
@@ -5,10 +5,12 @@ namespace TicTacToe
         public const int PlayerX = 1;
         public const int PlayerO = 2;
 
+        public const int IndexingAdjustment = 1;
+
         public const string ValidMoveRegularExpression = "^[1-3],[1-3]$";
 
         public const string InvalidInputErrorMessage = "Invalid Input, please try again: ";
-        public const string MoveAcceptedMessage = "Move Accepted, ";
+        public const string MoveAcceptedMessage = "Move Accepted.";
         public const string GameQuitMessage = "You have given up.\n";
 
         public const string QuitCommand = "q";
